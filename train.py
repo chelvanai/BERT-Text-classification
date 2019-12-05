fc609241ed44c593537efef5901c9bab70d47306
chelvan <chelvamchelvan@yahoo.com>
2019-12-05 11:20:00 +0530

    second

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-import re
 import tensorflow as tf
 import numpy as np
 import os
@@ -10,37 +9,37 @@ from tensorflow.keras.layers import LSTM, Embedding, Dense
 from tensorflow.keras.preprocessing import sequence
 
 
-# Load all files from a directory in a DataFrame.
-def load_directory_data(directory):
-    data = {"sentence": []}
-    for file_path in os.listdir(directory):
-        with open(directory + "/" + file_path) as f:
-            text = f.read()
-            words = set(tf.keras.preprocessing.text.text_to_word_sequence(text))
-            vocab_size = len(words)
-            result = tf.keras.preprocessing.text.one_hot(text, round(vocab_size * 1.3))
-            data['sentence'].append(result)
+# # Load all files from a directory in a DataFrame.
+# def load_directory_data(directory):
+#     data = {"sentence": []}
+#     for file_path in os.listdir(directory):
+#         with open(directory + "/" + file_path) as f:
+#             text = f.read()
+#             words = set(tf.keras.preprocessing.text.text_to_word_sequence(text))
+#             vocab_size = len(words)
+#             result = tf.keras.preprocessing.text.one_hot(text, round(vocab_size * 1.3))
+#             data['sentence'].append(result)
+#
+#     return pd.DataFrame.from_dict(data)
+#
+#
+# # Merge positive and negative examples, add a polarity column and shuffle.
+# def load_dataset():
+#     pos_df = load_directory_data('Data/train/pos')
+#     neg_df = load_directory_data('Data/train/neg')
+#     pos_df["polarity"] = 1
+#     neg_df["polarity"] = 0
+#     return pd.concat([pos_df, neg_df])
+#
+#
+# x_train = load_dataset()['sentence'].values
+# y_train = load_dataset()['polarity'].values
+#
+# x_train = sequence.pad_sequences(x_train, maxlen=2000)
+# y_train = utils.to_categorical(y_train, 2)
 
-    return pd.DataFrame.from_dict(data)
 
-
-# Merge positive and negative examples, add a polarity column and shuffle.
-def load_dataset():
-    pos_df = load_directory_data('Data/train/pos')
-    neg_df = load_directory_data('Data/train/neg')
-    pos_df["polarity"] = 1
-    neg_df["polarity"] = 0
-    return pd.concat([pos_df, neg_df])
-
-
-x_train = load_dataset()['sentence'].values
-y_train = load_dataset()['polarity'].values
-
-x_train = sequence.pad_sequences(x_train, maxlen=2000)
-y_train = utils.to_categorical(y_train, 2)
-
-
-# Create custom bert layer
+# Create custom BERT layer in keras
 class BertLayer(tf.keras.layers.Layer):
     def __init__(self, n_fine_tune_layers=10, **kwargs):
         self.n_fine_tune_layers = n_fine_tune_layers
@@ -49,11 +48,10 @@ class BertLayer(tf.keras.layers.Layer):
         super(BertLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.bert = hub.Module(
-            bert_path,
-            trainable=self.trainable,
-            name="{}_module".format(self.name)
-        )
+        self.bert = hub.Module("https://tfhub.dev/tensorflow/bert_en_cased_L-24_H-1024_A-16/1",
+                               trainable=self.trainable,
+                               name="{}_module".format(self.name))
+
         trainable_vars = self.bert.variables
 
         # Remove unused layers
@@ -73,15 +71,13 @@ class BertLayer(tf.keras.layers.Layer):
 
         super(BertLayer, self).build(input_shape)
 
-    def call(self, inputs):
-        inputs = [K.cast(x, dtype="int32") for x in inputs]
+    def calls(self, inputs):
+        inputs = [tf.keras.backend.cast(x, type="int32") for x in inputs]
         input_ids, input_mask, segment_ids = inputs
         bert_inputs = dict(
             input_ids=input_ids, input_mask=input_mask, segment_ids=segment_ids
         )
-        result = self.bert(inputs=bert_inputs, signature="tokens", as_dict=True)[
-            "pooled_output"
-        ]
+        result = self.bert(input=bert_inputs, signature="tokens", as_dict=True)
         return result
 
     def compute_output_shape(self, input_shape):
@@ -96,20 +92,3 @@ bert_inputs = [in_id, in_mask, in_segment]
 
 # Instantiate the custom Bert Layer defined above
 bert_output = BertLayer(n_fine_tune_layers=10)(bert_inputs)
-
-# Build the rest of the classifier
-dense = tf.keras.layers.Dense(256, activation='relu')(bert_output)
-pred = tf.keras.layers.Dense(1, activation='sigmoid')(dense)
-
-model = tf.keras.models.Model(inputs=bert_inputs, outputs=pred)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-print(model.summary())
-
-# model.fit(
-#     [train_input_ids, train_input_masks, train_segment_ids],
-#     train_labels,
-#     validation_data=([test_input_ids, test_input_masks, test_segment_ids], test_labels),
-#     epochs=1,
-#     batch_size=32
-# )
